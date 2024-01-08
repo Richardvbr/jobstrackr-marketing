8be9294e632fb3ef15f0a4ea34010b89cd1cf1b7
Richard van Brunschot <richardvanbrunschot@gmail.com>
2024-01-08 21:00:35 +0100

    Upgrade project dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import icon from 'astro-icon'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
-});
+	integrations: [tailwind({ applyBaseStyles: false }), icon()]
+})
